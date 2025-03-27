a0d0c172f68f125a7c578a612ceb0dad614a48bc
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-03-27 10:45:49 -0300

    feat: create components Cards

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,3 +1,22 @@
-export default function Header() {
+import styles from "../styles/Cards.module.css";
 
+export default function Cards({image, title, text, icon, valor, descricao}) {
+    return (
+       <div className={styles.cards}>
+        <Image className={styles.img} src={image} alt="imagem"/>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.name}>{text}</p>
+        <div className={styles.icons}>
+            <p>{icon}</p>
+        </div>
+        <div className={styles.descricao}>
+            <p>{descricao}</p>
+        </div>
+        <div className="fimcard">
+            <p>{valor}</p>
+            <p>{icon}</p>
+            <p>{icon}</p>  
+        </div>
+       </div>
+    )
 }
